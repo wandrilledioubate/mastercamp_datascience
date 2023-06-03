758af2c288b3e0642c4a1234cbc7f1e7cc6dbcc2
Wandrille Dioubaté <74004697+wandrilledioubate@users.noreply.github.com>
2023-06-03 17:24:35 +0200

    td1&2

--- td1.py
+++ td1.py
@@ -1,1 +1,297 @@
-print("Hello world")
+# %% Exercice  1 : calcul de la vitesse
+
+temps = 6.892
+distance= 19.7
+
+vitesse = distance / temps 
+print("La vitesse est de", vitesse)
+
+vitesse_2dec = "{:.2f}".format(vitesse)
+print("La vitesse est de", vitesse_2dec)
+
+
+
+# %% Exercice 2 - A : test à une alternative 
+
+a = 10
+b = 5
+
+if a > b:
+    maximum = a
+    minimum = b
+else:
+    maximum = b
+    minimum = a
+
+print("Le maximum est :", maximum)
+print("Le minimum est :", minimum)    
+
+
+
+# %% Exercice 2 - B : test simple
+
+
+a = 10
+b = 5
+
+maximum = a
+minimum = b
+
+if b > a:
+    maximum = b
+    minimum = a
+
+print("Le maximum est :", maximum)
+print("Le minimum est :", minimum)
+
+
+# %% Exercice 2 - C : test ternaire
+
+a = 22 
+b = 46
+
+maximum = a if a > b else b
+minimum = a if a < b else b
+
+print("Le maximum est :", maximum)
+print("Le minimum est :", minimum)
+    
+
+# %% Exercice 3 
+
+def volBoite(x1=None, x2=None, x3=None):
+    if x1 is None:
+        return None  
+
+    if x2 is None and x3 is None:
+        return x1 ** 3  
+
+    if x2 is not None and x3 is None:
+        return x1 ** 2 * x2  
+
+    if x2 is not None and x3 is not None:
+        return x1 * x2 * x3  
+
+    return None    
+    
+# Test de la fonction
+print (volBoite())
+print (volBoite(5.2))
+print (volBoite(5.2, 3))
+print (volBoite(5.2, 3, 7.4))
+
+# %% Exercice 4 
+
+def eleMax(liste, debut=None, fin=None):
+    if debut is None:
+        debut = 0
+    if fin is None:
+        fin = len(liste) - 1
+    
+    max_element = liste[debut]
+    for i in range(debut + 1, fin + 1):
+        if liste[i] > max_element:
+            max_element = liste[i]
+    
+    return max_element
+
+
+# Test de la fonction 
+serie = [9, 3, 6, 1, 7, 5, 4, 8, 2]
+
+print(eleMax(serie))
+print(eleMax(serie, 2, 5))
+print(eleMax(serie, 2))
+print(eleMax(serie, fin =3, debut =1))
+
+# %% Exercice 5 
+
+def mixList(list1, list2):
+    t3 = []
+    min_length = min(len(list1), len(list2))
+    
+    for i in range(min_length):
+        t3.append(list2[i])
+        t3.append(list1[i])
+
+    if len(list1) > len(list2):
+        t3.extend(list1[min_length:])
+    elif len(list2) > len(list1):
+        t3.extend(list2[min_length:])
+    
+    return t3
+    
+# Test de la fonction 
+t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t2 = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+
+print (mixList(t1, t2))
+
+# %% Exercice 6 
+
+def reverseList(chaine):
+    reverse = chaine[::-1]
+    return reverse
+
+# Test de la fonction 
+mot = 'fdnsfjdsnk'
+
+print(reverseList(mot))
+
+# %% Exercice 7 
+
+
+def mafonction(arr): 
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return(arr)
+
+# Test de la fonction
+array = [1,8,8,2,9,3,10,5]
+print(mafonction(array))                
+                
+# %% Exercice 8 
+
+def countdown(start):
+    for i in range(start, 0, -1):
+        print(i, end=' ')
+    print("GO!")
+    
+# Test de la fonction    
+nb = 3
+countdown(nb)
+
+# %% Exercice 9 
+
+def isPalindrome(word):
+    word = word.replace(" ", "")
+    reversed_word = word[::-1]
+    if word == reversed_word:
+        print(f"The word '{word}' is a palindrome.")
+    else:
+        print(f"The word '{word}' is not a palindrome.")
+
+
+#Test de la fonction 
+test = 'kayak'
+isPalindrome(test)
+    
+# %% Exercice 10 
+
+my_list = [0, 1, 2, 3, 4]
+l1 = my_list + my_list[::-1]
+l2 = my_list[:3] * 3
+l3 = [my_list[i] for i in range(len(my_list)) if i % 3 == 0]
+
+print("l1:", l1)
+print("l2:", l2)
+print("l3:", l3)
+
+# %% Exercice 11 
+
+x = int(input("Enter an integer: "))
+
+if x % 2 == 0:
+    print("pair")
+else:
+    print("impair")
+
+# %% Exercice 12 
+
+phrase = input("Enter a phrase: ")
+
+words = phrase.split()
+acronym = ""
+for word in words:
+    acronym += word[0].upper()
+
+print(acronym)
+
+# %% Exercice 13 
+
+chaine = input("Enter a string: ")
+c = input("Enter a character: ")
+
+count = chaine.count(c)
+
+print(f"'{c}' apparaît {count} fois dans \"{chaine}\"")
+
+# %% Exercice 14 
+
+def vowelCount(string):
+    string = string.lower()
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    count = 0
+    for char in string:
+        if char in vowels:
+            count += 1
+    return count
+
+mot = 'maison'
+vowelCount(mot)
+
+# %% Exercice 15 
+
+my_dict = {
+    "pi": 3.14,
+    "mot": "mot",
+    "nombre": 42,
+    "liste": [1, 2, 3]
+}
+
+print(my_dict["pi"])  
+print(my_dict["mot"])  
+print(my_dict["nombre"])  
+print(my_dict["liste"])  
+
+# %% Exercice 16 
+
+my_dict = {
+    "pi": 3.14,
+    "mot": "mot",
+    "nombre": 42,
+    "liste": [1, 2, 3]
+}
+
+word = input("Enter a word: ")
+
+if word in my_dict:
+    value = my_dict[word]
+    print(f"{word} vaut {value} dans my_dict")
+else:
+    print(f"{word} n'est pas une clé de my_dict")
+    
+    
+# %% Exercice 17 
+
+my_dict = {
+    "pi": 3.14,
+    "mot": "mot",
+    "nombre": 42,
+    "liste": [1, 2, 3]
+}
+
+my_dict["hello"] = "world"
+my_dict["nombre"] = 0
+del my_dict["pi"]
+
+print(my_dict)
+
+
+# %% Exercice 18 
+
+values = [10, 20, 30, 40, 10, 2, 40]
+
+s1 = {1, 2, 3}
+s2 = set("Hello World")
+s3 = set(values)
+s4 = set(range(5, 16))
+
+print("s1:", s1)
+print("s2:", s2)
+print("s3:", s3)
+print("s4:", s4)
+
---
+++ td2.py
@@ -0,0 +1,279 @@
+# %% Exercice 1 
+
+ch = "12 13 10 8 20"
+my_list = list(map(int, ch.split()))
+print(my_list)
+
+# %% Exercice 2 
+
+test = [1, 2, 3, 4, 6]
+values1 = ['a', 'b', 'c', 'd', 'e']
+values2 = ['A', 'B', 'C', 'D', 'E']
+
+result = tuple(map(lambda x, y, z: y if x % 2 != 0 else z, test, values1, values2))
+print(result)
+
+# %% Exercice 3 
+
+N = 2
+my_list = [2 ** i for i in range(N)]
+print(my_list)
+
+# %% Exercice 4 
+
+numbers=[-1,-2,-3,4,5,10,-5,3]
+
+my_list = [x ** 2 for x in numbers if x < 0]
+print(my_list)
+
+
+# %% Exercice 5 
+
+n = 14 
+x = 2
+my_list = [i for i in range(1,n) if i % x != 0]
+print(my_list)
+
+
+# %% Exercice 6
+
+revert = ' '.join(sentence[::-1] for sentence in input("Entrez votre phrase: ").split())
+print(revert)
+
+
+# %% Exercice 7  
+
+words = ' '.join(sentence for sentence in input("Entrez votre phrase: ").split() if sentence.islower() and sentence.isalpha())
+print(words)
+
+# %% Exercice 8 
+
+s1 = "Kit Surf en Mer"
+s2 = "Maison en Bois avec Surf"
+
+s1_words = s1.lower().split()
+s2_words = s2.lower().split()
+
+common_words = set(s1_words) & set(s2_words)
+
+s1 = ' '.join(word.upper() if word in common_words else word for word in s1_words)
+s2 = ' '.join(word.upper() if word in common_words else word for word in s2_words)
+
+print(s1)
+print(s2)
+
+# %% Exercice 9 
+
+dico={'b':1, 'a':2, 'd':5}
+revert_dico = {x: y for y, x in dico.items()} 
+print(revert_dico)
+
+# %% Exercice 10 
+
+my_dict = {"a":"b", "b":"c", "c":"X"}
+correspondances = {"a":"A", "b":"D", "c":"C"}
+results = {k: correspondances[v] for k, v in my_dict.items() if v in correspondances}
+print(results)
+
+
+# %% Exercice 11 - A
+
+def sortedList(liste):
+    liste.sort()
+    return liste 
+
+# Test de la fonction 
+l1=[5,3,10,4,8,20,2]
+print(sortedList(l1))
+
+
+# %% Exercice 11 - B
+
+def sortedSizeWord(liste):
+    liste.sort(key=lambda v: len(v))
+    return liste
+
+# Test de la fonction 
+l2 = ["bonjour","tout","le","monde"]
+print(sortedSizeWord(l2))
+
+
+# %% Exercice 11 - C
+
+def sortedDescMax(liste):
+    liste.sort(key=max, reverse=True)
+    return liste
+
+
+# Test de la fonction 
+l3 =[ [1,0,5],[1,2,3], [8,5] ]
+print(sortedDescMax(l3))
+
+
+# %% Exercice 11 - D
+
+def sortedTuple(liste):
+    liste.sort(key=lambda x: x[0])
+    print("Liste triée par ordre alphabétique : ", liste)
+    liste.sort(key=lambda x: x[1])
+    print("Liste triée par âge : ", liste)   
+    
+# Test de la fonction 
+l4=[('Luc',22),('Lea',18),('Alice',23),('Luca',21), ('Max',20) ]
+print(sortedTuple(l4))
+
+# %% Exercice 12 
+
+def boomerang(iterable):
+    items = list(iterable)
+    yield from items
+    yield from reversed(items)
+
+# Test de la fonction 
+l = [1, 2, 3]
+for x in boomerang(l):
+    print(x)
+    
+# %% Partie 2 - A 
+
+# Le constructeur est une méthode spéciale def __init__ qui est utilisée pour initialiser les objets lors de leur création.
+
+# Les variables d'instances sont des variables qui sont spécifiques à chaque objet créé à partir de la classe. Dans la classe CompteBancaire, les variables d'instances sont :
+  # self.nom
+  # self.solde
+  # self.iban 
+
+
+
+
+# %% Partie 2 - B 
+
+
+class Point3D:
+    def __init__(self, x, y, z):
+        self.coordinates = (x, y, z)
+
+    def __str__(self):
+        return f"Point3D({self.coordinates[0]}, {self.coordinates[1]}, {self.coordinates[2]})"
+
+    def __abs__(self):
+        return (self.coordinates[0] ** 2 + self.coordinates[1] ** 2 + self.coordinates[2] ** 2) ** 0.5
+
+    def __add__(self, other):
+        if isinstance(other, Point3D):
+            x = self.coordinates[0] + other.coordinates[0]
+            y = self.coordinates[1] + other.coordinates[1]
+            z = self.coordinates[2] + other.coordinates[2]
+            return Point3D(x, y, z)
+        else:
+            raise TypeError("L'opération d'addition n'est pas prise en charge pour les types fournis.")
+
+    def __sub__(self, other):
+        if isinstance(other, Point3D):
+            x = self.coordinates[0] - other.coordinates[0]
+            y = self.coordinates[1] - other.coordinates[1]
+            z = self.coordinates[2] - other.coordinates[2]
+            return Point3D(x, y, z)
+        else:
+            raise TypeError("L'opération de soustraction n'est pas prise en charge pour les types fournis.")
+
+    def __eq__(self, other):
+        if isinstance(other, Point3D):
+            return self.coordinates == other.coordinates
+        else:
+            return False
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            x = self.coordinates[0] * other
+            y = self.coordinates[1] * other
+            z = self.coordinates[2] * other
+            return Point3D(x, y, z)
+        elif isinstance(other, Point3D):
+            x = self.coordinates[1] * other.coordinates[2] - self.coordinates[2] * other.coordinates[1]
+            y = self.coordinates[2] * other.coordinates[0] - self.coordinates[0] * other.coordinates[2]
+            z = self.coordinates[0] * other.coordinates[1] - self.coordinates[1] * other.coordinates[0]
+            return Point3D(x, y, z)
+        else:
+            raise TypeError("L'opération de multiplication n'est pas prise en charge pour les types fournis.")
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __getitem__(self, index):
+        return self.coordinates[index]
+
+    def __setitem__(self, index, value):
+        new_coordinates = list(self.coordinates)
+        new_coordinates[index] = value
+        self.coordinates = tuple(new_coordinates)
+
+# Test de la classe Point3D
+p1 = Point3D(1, 2, 3)
+p2 = Point3D(4, 5, 6)
+
+print(p1)  
+print(p2)
+
+distance = abs(p1 - p2)
+print(distance)  
+
+p3 = p1 + p2
+print(p3)  
+
+p4 = p2 - p1
+print(p4) 
+
+print(p1 == p2)  
+
+p5 = p1 * 2
+print(p5) 
+
+p6 = p1 * p2
+print(p6) 
+
+p1[0] = 10
+print(p1) 
+
+
+# %% Partie 2 - C
+
+
+class Mass3D(Point3D):
+    def __init__(self, x, y, z, m):
+        super().__init__(x, y, z)
+        self.mass = m
+
+    def __str__(self):
+        return f"Mass3D({self.coordinates[0]}, {self.coordinates[1]}, {self.coordinates[2]}, {self.mass})"
+
+# Test de la classe Mass3D  
+mass_point = Mass3D(1, 2, 3, 10)
+print(mass_point)  # Affiche Mass3D(1, 2, 3, 10)
+    
+  
+m1 = Mass3D(1, 2, 3, 10)
+m2 = Mass3D(4, 5, 6, 5)
+
+print(m1) 
+print(m2) 
+
+distance_m = abs(m1 - m2)
+print(distance_m)  
+
+m3 = m1 + m2
+print(m3) 
+
+m4 = m2 - m1
+print(m4) 
+
+print(m1 == m2)  
+
+m5 = m1 * 2
+print(m5)
+
+m6 = m1 * m2
+print(m6)  
+
+m1[0] = 10
+print(m1)  
